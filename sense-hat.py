668a535e1daa694ddeb572cdc6e07e5a6d9d205b
Vobistein <92033540+vobistein@users.noreply.github.com>
2025-03-21 16:12:22 +0100

    add colors

---
+++ sense-hat.py
@@ -0,0 +1,14 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+sense.show_message("Hello world")
+
+g = (0, 255, 0)
+y = (255, 255, 0)
+b = (0, 0, 255)
+r = (255, 0, 0)
+w = (255,255,255)
+n = (0,0,0)
+p = (255,105, 180)
+
