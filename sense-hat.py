75f5bcb7ea75483a7d7910e1f72b062eaab98946
Vobistein <92033540+vobistein@users.noreply.github.com>
2025-03-21 17:58:25 +0100

    fix 12

--- sense-hat.py
+++ sense-hat.py
@@ -51,7 +51,7 @@ sense.stick.direction_middle = middle
 while True:
     match mode:
         case "color":
-            #framebuffer[1] = sense.colour.colour # return tuple of (r,g,b,clear)
+            framebuffer[1] = sense.colour.colour[:3] # return tuple of (r,g,b,clear)
             print(sense.colour.colour)
             sense.colour.integration_time
         case "pressure":
