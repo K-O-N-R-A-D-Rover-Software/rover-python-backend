e8d24391bb6ff91ccc79b3a17424230f9af75bbb
duoistwo <ralt1@posteo.de>
2025-04-23 18:28:44 +0200

    many changes

---
+++ startDataServer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/rover/rover-python-backend/dataServer
+source venv/bin/activate
+python main.py
---
+++ startMotorController.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/rover/rover-python-backend/motorController/
+source venv/bin/activate
+python motorcontrol.py
